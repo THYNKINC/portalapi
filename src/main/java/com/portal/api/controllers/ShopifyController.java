d6ba8627cafbb3a19547c1fb37d95e55bb83d424
Marko Zugic <markozugic4@gmail.com>
2024-09-30 19:38:33 +0200

    added shopify webhook

--- src/main/java/com/portal/api/controllers/ShopifyController.java
+++ src/main/java/com/portal/api/controllers/ShopifyController.java
@@ -4,17 +4,17 @@ import com.portal.api.model.Order;
 import com.portal.api.repositories.OrderRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/protal/shopify")
+@RequestMapping("/shopify")
 public class ShopifyController {
 
     private static final Logger logger = LoggerFactory.getLogger(ShopifyController.class);
+
+    @Value("${shopify.webhook.signature}")
+    private String shopifySignature;
 
     private final OrderRepository orderRepository;
 
@@ -23,9 +23,13 @@ public class ShopifyController {
     }
 
     @PostMapping("/orders")
-    public void createOrder(@RequestBody String orderAsJson) {
-
+    public void createOrder(@RequestBody String orderAsJson, @RequestHeader("X-Shopify-Hmac-Sha256") String shopifySignatureHeader) {
         logger.info("Shopify order {}", orderAsJson);
+        logger.info("Shopify X-Shopify-Hmac-Sha256 {}", shopifySignatureHeader);
+
+        if (!shopifySignature.equals(shopifySignatureHeader)) {
+            return;
+        }
 
         Order order = new Order();
         order.setOrder(orderAsJson);
