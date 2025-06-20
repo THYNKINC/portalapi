20d8c5b5a95fd04fa0a98918c624b90e3c2a2885
Josh Russell <jrussel@metisminds.com>
2025-06-20 17:35:31 +0000

    Squashed commit of the following
    commit b9d570fa 
    Author: Josh Russell <josh@launchcodeconsulting.com> 
    Date: Fri Jun 20 2025 13:22:43 GMT-0400 (Eastern Daylight Time) 
    
        add weights to impulse control, update focus weights

--- src/main/java/com/portal/api/model/ImpulseControl.java
+++ src/main/java/com/portal/api/model/ImpulseControl.java
@@ -16,19 +16,19 @@ public class ImpulseControl {
 	private String session;
 	private double focus;
 	private double impulse;
-	
+
 	public static ImpulseControl fromSkills(String attempt, CognitiveSkillsResponse skills, int missionNo) {
-		
+
 		double focus = 0;
 		int fa = skills.getFocusedAttention();
 		int sa = skills.getSustainedAttention();
-		
+
 		focus = (fa > 0 && sa > 0) ? (fa + sa) / 2 : Math.max(fa, sa);
 		focus = Math.max(focus, 0);
 
 		// apply weights to composite focus
-		focus *= Constants.COMPOSITE_SCORE_WEIGHT[missionNo - 1];
-		
+		focus *= Constants.FOCUS_COMPOSITE_SCORE_WEIGHT[missionNo - 1];
+
 		OptionalDouble impulse = IntStream
 				.of(
 						skills.getSelectiveAttention(),
@@ -39,7 +39,11 @@ public class ImpulseControl {
 						skills.getInterferenceControl())
 				.filter(i -> i >= 0)
 				.average();
-		
-		return new ImpulseControl(attempt, focus, impulse.orElse(-1));
+
+		double impulseValue = impulse.orElse(-1);
+		if (impulseValue != -1) {
+			impulseValue *= Constants.IMPULSE_COMPOSITE_SCORE_WEIGHT[missionNo - 1];
+		}
+		return new ImpulseControl(attempt, focus, impulseValue);
 	}
 }
--- src/main/java/com/portal/api/util/Constants.java
+++ src/main/java/com/portal/api/util/Constants.java
@@ -2,5 +2,6 @@ package com.portal.api.util;
 
 public abstract class Constants {
 
-	public static final double[] COMPOSITE_SCORE_WEIGHT = new double[] {0, 1.0, 1.2, 1.5, 1.7, 1.9, 3.5, 3.7, 3.9, 2.5, 2.7, 2.9, 1.9, 3.9, 2.9};
+	public static final double[] FOCUS_COMPOSITE_SCORE_WEIGHT = new double[] {0, 1.0, 1.2, 1.5, 1.7, 1.9, 3.0, 3.2, 3.4, 2.5, 2.7, 2.9, 1.9, 3.4, 2.9};
+	public static final double[] IMPULSE_COMPOSITE_SCORE_WEIGHT = new double[] {0, 1.0, 1.1, 1.3, 1.4, 1.5, 1.8, 1.9, 2.0, 2.3, 2.4, 2.5, 2.8, 2.9, 3.0};
 }
